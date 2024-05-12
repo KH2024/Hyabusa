65a3e003bdbfbb7133bdb1457f9c5889c2df31a3
sujoy126 <sujoyoffice126@gmail.com>
2024-05-12 20:05:11 +0530

    abc

---
+++ src/test/java/com/parlr/TestCases/tc3Test.java
@@ -0,0 +1,14 @@
+package com.parlr.TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc3Test {
+	@Test
+	public void mmm() {
+	Reporter.log("Hello Qspider");
+	}
+
+}
